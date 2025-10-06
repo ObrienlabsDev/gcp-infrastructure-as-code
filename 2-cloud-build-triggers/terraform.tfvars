33e9212f9de3f2607b51e32deefecf849d6919aa
Michael O'Brien <michael@obrienlabs.org>
2025-10-06 12:25:33 -0400

    #2 - cb trigger start

--- 2-cloud-build-triggers/terraform.tfvars
+++ 2-cloud-build-triggers/terraform.tfvars
@@ -1,1 +1,3 @@
 // stub
+project_id="gcp-infrastructure-as-code"
+secret_id="github_2_test"
--- 2-cloud-build-triggers/variables.tf
+++ 2-cloud-build-triggers/variables.tf
@@ -1,1 +1,10 @@
 // stub
+// SECRET_ID is the ID of your token or secret in Secret Manager.
+variable "secret_id" {
+  type        = string
+}
+
+// PROJECT_ID is your Google Cloud project ID.
+variable "project_id" {
+  type        = string
+}
