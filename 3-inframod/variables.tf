dcc24b0c8ecc895d14e8e35fa58ecf331b57b1f3
Michael O'Brien <michael@obrienlabs.org>
2025-10-10 12:25:56 -0400

    #6 - start vpc terraform

--- 3-inframod/variables.tf
+++ 3-inframod/variables.tf
@@ -1,4 +1,9 @@
 // stub
+// PROJECT_ID is your Google Cloud project ID.
+variable "project_id" {
+  type        = string
+}
+
 // SECRET_ID is the ID of your token or secret in Secret Manager.
 variable "secret_id" {
   type        = string
