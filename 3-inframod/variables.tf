31355158e3b67b8421513d5e82f976a61537efa2
Michael O'Brien <michael@obrienlabs.org>
2025-10-10 13:08:50 -0400

    #6 - start vpc terraform

--- 3-inframod/variables.tf
+++ 3-inframod/variables.tf
@@ -1,10 +1,9 @@
 // stub
-// PROJECT_ID is your Google Cloud project ID.
+
 variable "project_id" {
   type        = string
 }
 
-// SECRET_ID is the ID of your token or secret in Secret Manager.
 variable "secret_id" {
   type        = string
 }
--- 3-inframod/vpc.tf
+++ 3-inframod/vpc.tf
@@ -1,5 +1,5 @@
 resource "google_compute_network" "vpc_network" {
-  project                 = var.project_id # Replace this with your project ID in quotes
+  project                 = var.project_id 
   name                    = var.vpc_name
   auto_create_subnetworks = false
   mtu                     = 1460
