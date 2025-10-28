be370fcc5ccc533049b4293dac063f403c614668
Michael OBrien <michael@obrienlabs.org>
2025-10-28 16:20:19 -0400

    #10 - gke ingress stubs

--- m1-gke-ingress/apply.sh
+++ m1-gke-ingress/apply.sh
@@ -1,6 +1,33 @@
 #!/bin/bash
 
+  source vars.sh
 
-export CONTACT_DOMAIN=obrienlabs.dev
-export SUPER_ADMIN_EMAIL=michael@$CONTACT_DOMAIN
-echo $SUPER_ADMIN_EMAIL
+  PROJECT_ID=biometric-backend-gke-man-old
+  ORGANIZATION_ID=$(gcloud projects get-ancestors $PROJECT_ID --format='get(id)' | tail -1)
+  echo "Derived organization_id: $ORGANIZATION_ID"
+  BILLING_ID=$(gcloud alpha billing projects describe $PROJECT_ID '--format=value(billingAccountName)' | sed 's/.*\///')
+  echo "Derived billing_id: $BILLING_ID"
+  
+  gcloud services enable container.googleapis.com
+  gcloud services enable compute.googleapis.com
+
+  gcloud beta container /
+    --project "biometric-backend-gke-man-old" /
+    clusters create-auto "dev" /
+    --region "northamerica-northeast1" /
+    --release-channel "stable" /
+    --enable-private-nodes /
+    --enable-dns-access /
+    --enable-k8s-tokens-via-dns /
+    --enable-k8s-certs-via-dns /
+    --enable-ip-access /
+    --enable-master-global-access /
+    --enable-google-cloud-access /
+    --network "projects/biometric-backend-gke-man-old/global/networks/default" /
+    --subnetwork "projects/biometric-backend-gke-man-old/regions/northamerica-northeast1/subnetworks/default" /
+    --cluster-ipv4-cidr "10.0.0.0/17" /
+    --binauthz-evaluation-mode=DISABLED /
+    --scopes=https://www.googleapis.com/auth/cloud-platform /
+    --enable-secret-manager
+
+
--- m1-gke-ingress/vars.sh
+++ m1-gke-ingress/vars.sh
@@ -1,6 +1,51 @@
 #!/bin/bash
 
+# Copyright 2025 ObrienLabs Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-export CONTACT_DOMAIN=obrienlabs.dev
-export SUPER_ADMIN_EMAIL=michael@$CONTACT_DOMAIN
-echo $SUPER_ADMIN_EMAIL
+###
+# UPDATE
+export REGION=northamerica-northeast1
+#export REGION=us-central1
+
+#export VAR_GOOGLE_PROCESSOR_ID=491b42bf46846175
+
+
+# current project - not the target project
+#export BOOT_PROJECT_ID=$PROJECT_ID
+export BOOT_PROJECT_ID=gcp-infrastructure-as-code
+# generated project prefix
+export KCC_PROJECT_NAME=gcp-infrastructure-as-code
+# used for vpc, subnet, KCC cluster
+export PREFIX=old
+
+
+# repo from where we run the scripts
+export SRC_REPO_STAGING=gcp-infrastructure-as-code
+# for non gerrit
+#export GITHUB_ORG=GoogleCloudPlatform
+export GITHUB_ORG=ObrienlabsDev
+
+# branch under use
+#export CSR_BRANCH_OTHER_THAN_MAIN=canary
+export CSR_BRANCH_OTHER_THAN_MAIN=main
+export NETWORK=$REGION
+export SUBNET=$NETWORK-sn
+export ZONE=$REGION-a
+export VPC=$NETWORK-vpc
+
+export CIDR_VPC=10.0.0.0/16
+# see cloud build yamls
+export GKE_PROD=gke-prod
+export BRANCH_PROD=main
