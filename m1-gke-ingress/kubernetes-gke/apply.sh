55b722fd408d8631948f085f7e65f00c02faeb50
Michael OBrien <michael@obrienlabs.org>
2025-11-02 21:13:01 -0500

    #10 - gke standard cluster

--- m1-gke-ingress/kubernetes-gke/apply.sh
+++ m1-gke-ingress/kubernetes-gke/apply.sh
@@ -18,16 +18,16 @@
 #gcloud compute routers create my-router --region us-central1 --network default --project=biometric-backend-gke-man-old
 #gcloud beta compute routers nats create nat --router=my-router --region=us-central1 --auto-allocate-nat-external-ips --nat-all-subnet-ip-ranges --project=biometric-backend-gke-man-old
 
-
 # manual
-gcloud beta container --project "biometric-backend-gke-man-old" clusters create "dev2-man" --region "us-central1" --no-enable-basic-auth --cluster-version "1.34.1-gke.1829001" \
+gcloud beta container --project "biometric-backend-gke-man-old" clusters create "dev2-man" --zone "us-central1-a" --no-enable-basic-auth --cluster-version "1.34.1-gke.1829001" \
   --release-channel "rapid" --machine-type "e2-medium" --image-type "COS_CONTAINERD" --disk-type "pd-balanced" --disk-size "80" --metadata disable-legacy-endpoints=true \
   --scopes "https://www.googleapis.com/auth/cloud-platform" --num-nodes "3" --logging=NONE --enable-ip-alias \
   --network "projects/biometric-backend-gke-man-old/global/networks/dev-man" --subnetwork "projects/biometric-backend-gke-man-old/regions/us-central1/subnetworks/dev-man-sn" \
   --cluster-ipv4-cidr "10.1.0.0/17" --no-enable-intra-node-visibility --default-max-pods-per-node "110" --enable-ip-access --security-posture=standard --workload-vulnerability-scanning=disabled \
   --enable-dataplane-v2 --no-enable-google-cloud-access --addons HorizontalPodAutoscaling,HttpLoadBalancing,GcePersistentDiskCsiDriver --enable-autoupgrade --enable-autorepair \
-  --max-surge-upgrade 1 --max-unavailable-upgrade 0 --binauthz-evaluation-mode=DISABLED --no-enable-managed-prometheus --enable-secret-manager \
-  --enable-secret-manager-rotation --secret-manager-rotation-interval "120s" --shielded-integrity-monitoring --no-shielded-secure-boot
+  --max-surge-upgrade 1 --max-unavailable-upgrade 0 --binauthz-evaluation-mode=DISABLED --no-enable-managed-prometheus --shielded-integrity-monitoring --no-shielded-secure-boot \
+      --node-locations "us-central1-a"
+#--enable-secret-manager --enable-secret-manager-rotation --secret-manager-rotation-interval "120s" \
 
 #kubectl apply -f namespace.yaml
 #kubectl apply -f deployment.yaml
