831d9a1708cc14ddae9a3a999cf5b94ed8753e12
Michael O'Brien <michael@obrienlabs.org>
2026-01-19 17:12:37 -0500

    #16 - Update ollama-gpt-oss.sh

--- m10-3rdparty/m11-nvidia-gce-terraform-ubuntu/ollama-gpt-oss.sh
+++ m10-3rdparty/m11-nvidia-gce-terraform-ubuntu/ollama-gpt-oss.sh
@@ -1,8 +1,12 @@
 nvidia-smi
-curl -fsSL https://ollama.com/install.sh | sh
+# before starting the server
 # for evantual multiple L4's
 export OLLAMA_SCHED_SPREAD=1
 export ROCR_VISIBLE_DEVICES=0,1
 export CUDA_VISIBLE_DEVICES=0,1
+
+# install and start the server
+curl -fsSL https://ollama.com/install.sh | sh
+
 # L4 currently is running 24G vram for Ada - up from 16G for Ampere
 ollama run gpt-oss:20b --verbose
