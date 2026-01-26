2d67ad672a719a904ef6891f3959fb2bd7dcb0b6
Michael OBrien <michael@obrienlabs.org>
2026-01-26 02:25:17 +0000

    #21 - folder deletion_protection

--- m20-terraform-change-sandbox/folder.tf
+++ m20-terraform-change-sandbox/folder.tf
@@ -1,5 +1,7 @@
 resource "google_folder" "subfolder" {
   display_name = "subfolder"
   parent       = "folders/${var.parent_folder_id}"
-
+  # only empty folders can be deleted - if there are out-of-band sub projects or folders - terraform destroy will fail as expected
+  # "@type": "type.googleapis.com/google.rpc.PreconditionFailure","description": "The folder being deleted is non-empty.",
+  deletion_protection = false # Explicitly set to false
 }
